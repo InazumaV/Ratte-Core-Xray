290f3b64e61e5a67101fc7719ff34f59d3c0c6c4
Yuzuki616 <yuzuki2332@gmail.com>
2025-03-24 20:37:42 +0900

    fix: mistake

--- node_test.go
+++ node_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestXray_addRulesRouting_AND_delRulesRouting_(t *testing.T) {
+func TestXray_addRulesRouting_AND_delRulesRouting(t *testing.T) {
 	rs := []string{"domain:a.com", "domain:b.com"}
 	err := x.addRulesRouting(rs)
 	if err != nil {
