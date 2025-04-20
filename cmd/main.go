4b08f7915cbf4330d5c66d6d5c8941f911075a74
Yuzuki616 <yuzuki2332@gmail.com>
2025-04-20 10:20:24 +0900

    fix: error

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	xray "github.com/InazumaV/Ratte-Core-Xray"
 	"github.com/InazumaV/Ratte-Interface/core"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	c, err := core.NewServer(nil, NewXray())
+	c, err := core.NewServer(nil, xray.NewXray())
 	if err != nil {
 		log.Fatalln(err)
 	}
